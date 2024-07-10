43e0b4af20ae5ecf11943d7608dfe50591b26d15
cesrra <c3z4r.rincon@gmail.com>
2024-07-10 12:46:00 -0500

    feat: created the components with the ng-cli

---
+++ src/app/search/search-results/item-video/item-video.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-item-video',
+  standalone: true,
+  imports: [],
+  templateUrl: './item-video.component.html',
+  styleUrl: './item-video.component.scss'
+})
+export class ItemVideoComponent {
+
+}
